320aa33f145c08ab7a0e1c9581e677e84fd845a4
Sheyvie <sheilayvi@gmail.com>
2022-06-19 17:41:22 +0300

    created a virtual environment and added a module to created app

---
+++ I4G004355SOJ/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Post(models.Model):
+ Title= models.CharField(max_length=200)
+ Text= models.TextField()
+ Author= models.ForeignKey( settings.AUTH_USER_MODEL, on_delete=models.CASCADE, )
+ Created_date= models.DateTimeField()
+ Published_date= models.DateTimeField()
+ 
+ def __str__(self)-> str:
+  return self.Title
